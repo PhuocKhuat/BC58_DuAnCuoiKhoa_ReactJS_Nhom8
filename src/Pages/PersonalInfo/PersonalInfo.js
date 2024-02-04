18e5c1fc01b19f1a3dca32a4ba0353e464d2f47d
Phuoc <khuathoduyphuoc2001@gmail.com>
2024-02-04 23:44:25 +0700

    Xong phần skill, đang làm score trong personalInfo

--- src/Pages/PersonalInfo/PersonalInfo.js
+++ src/Pages/PersonalInfo/PersonalInfo.js
@@ -4,6 +4,8 @@ import "./stylePersonalInfo.css";
 import { https } from "../../Services/api";
 import { useDispatch, useSelector } from "react-redux";
 import { setInfoUser } from "../../Redux/personalSlice";
+import { Progress } from "antd";
+import {FieldTimeOutlined, BookOutlined, SwitcherOutlined, LayoutOutlined, BarChartOutlined, FileProtectOutlined} from "@ant-design/icons";
 
 export default function PersonalInfo() {
   const { infoUser } = useSelector((state) => state.personalSlice);
@@ -21,9 +23,9 @@ export default function PersonalInfo() {
       });
   }, []);
   return (
-    <section className="text-gray-600 body-font">
+    <section className="text-gray-600 body-font personalInfo">
       <div className="mb-5 sflex flex-col">
-        <div className="lg:w-4/6 mx-auto">
+        <div className="">
           <div className="flex flex-col sm:flex-row mt-10">
             <div className="sm:w-1/3 text-center sm:pr-8 sm:py-8">
               <div className="w-40 h-40 rounded-full inline-flex items-center justify-center bg-gray-200 text-gray-400">
@@ -43,8 +45,8 @@ export default function PersonalInfo() {
               </div>
             </div>
             <div className="sm:w-2/3 sm:pl-8 sm:py-8 sm:border-l border-gray-200 sm:border-t-0 border-t mt-4 pt-4 sm:mt-0 sm:text-left">
-              <div className="leading-relaxed text-lg mb-4 flex justify-between">
-                <div>
+              <div className="leading-relaxed text-lg mb-4 flex">
+                <div className="space-y-3">
                   <div>
                     <p>
                       <strong>Full Name: </strong> <span>{infoUser.hoTen}</span>
@@ -62,7 +64,7 @@ export default function PersonalInfo() {
                     </p>
                   </div>
                 </div>
-                <div>
+                <div className="space-y-3 ms-44">
                   <div>
                     <p>
                       <strong>Account: </strong>{" "}
@@ -82,20 +84,196 @@ export default function PersonalInfo() {
                   </div>
                 </div>
               </div>
-              <a className="text-indigo-500 inline-flex items-center">
-                Learn More
-                <svg
-                  fill="none"
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  className="w-4 h-4 ml-2"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M5 12h14M12 5l7 7-7 7" />
-                </svg>
-              </a>
+              <div>
+                <div>
+                  <h4 className="font-bold text-2xl">MY SKILLS</h4>
+                </div>
+                <div>
+                  <div className="grid grid-cols-12 space-x-5">
+                    <div className="col-span-6">
+                      <div className="flex items-center p-3">
+                        <p className="skillBtn skillBtnHtml">
+                          <span>html</span>
+                        </p>
+                        <Progress
+                          percent={100}
+                          status="active"
+                          strokeColor="#1677ff"
+                          className="progressInfoHtml"
+                          showInfo={false}
+                          size={[300, 20]}
+                        />
+                      </div>
+                      <div className="flex items-center p-3">
+                        <p className="skillBtn skillBtnCss">
+                          <span>css</span>
+                        </p>
+                        <Progress
+                          percent={85}
+                          status="active"
+                          strokeColor="#ff4560"
+                          className="progressInfoCss"
+                          showInfo={false}
+                          size={[300, 20]}
+                        />
+                      </div>
+                      <div className="flex items-center p-3">
+                        <p className="skillBtn skillBtnGit">
+                          <span>Git</span>
+                        </p>
+                        <Progress
+                          percent={90}
+                          status="active"
+                          strokeColor="#feb019"
+                          className="progressInfoGit"
+                          showInfo={false}
+                          size={[300, 20]}
+                        />
+                      </div>
+                      <div className="flex items-center p-3">
+                        <p className="skillBtn skillBtnJs">
+                          <span>JS</span>
+                        </p>
+                        <Progress
+                          percent={80}
+                          status="active"
+                          strokeColor="#00e396"
+                          className="progressInfoJs"
+                          showInfo={false}
+                          size={[300, 20]}
+                        />
+                      </div>
+                      <div className="flex items-center p-3">
+                        <p className="skillBtn skillBtnApi">
+                          <span>API</span>
+                        </p>
+                        <Progress
+                          percent={92}
+                          status="active"
+                          strokeColor="#775dd0"
+                          className="progressInfoApi"
+                          showInfo={false}
+                          size={[300, 20]}
+                        />
+                      </div>
+                    </div>
+                    <div className="col-span-6">
+                      <div className="flex items-center p-3">
+                        <p className="skillBtn skillBtnEs6">ES6</p>
+                        <Progress
+                          percent={75}
+                          status="active"
+                          strokeColor="#1677ff"
+                          className="progressInfoEs6"
+                          showInfo={false}
+                          size={[300, 20]}
+                        />
+                      </div>
+                      <div className="flex items-center p-3">
+                        <p className="skillBtn skillBtnReact">
+                          <span>React</span>
+                        </p>
+                        <Progress
+                          percent={98}
+                          status="active"
+                          strokeColor="#ff4560"
+                          className="progressInfoReact"
+                          showInfo={false}
+                          size={[300, 20]}
+                        />
+                      </div>
+                      <div className="flex items-center p-3">
+                        <p className="skillBtn skillBtnExpress">
+                          <span>Express</span>
+                        </p>
+                        <Progress
+                          percent={82}
+                          status="active"
+                          strokeColor="#feb019"
+                          className="progressInfoExpress"
+                          showInfo={false}
+                          size={[300, 20]}
+                        />
+                      </div>
+                      <div className="flex items-center p-3">
+                        <p className="skillBtn skillBtnNode">
+                          <span>Node</span>
+                        </p>
+                        <Progress
+                          percent={79}
+                          status="active"
+                          strokeColor="#00e396"
+                          className="progressInfoNode"
+                          showInfo={false}
+                          size={[300, 20]}
+                        />
+                      </div>
+                      <div className="flex items-center p-3">
+                        <p className="skillBtn skillBtnNest">
+                          <span>Nest</span>
+                        </p>
+                        <Progress
+                          percent={72}
+                          strokeColor="#775dd0"
+                          status="active"
+                          className="progressInfoNest"
+                          showInfo={false}
+                          size={[300, 20]}
+                        />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div>
+                <div>
+                  <h4>MY SCORES</h4>
+                </div>
+                <div className="timeStudy">
+                  <div className="timeStudyItem">
+                  <FieldTimeOutlined />
+                  <div>
+                    <h6>Study of hours</h6>
+                    <p>80</p>
+                  </div>
+                  </div>
+                  <div className="timeStudyItem">
+                  <BookOutlined />
+                  <div>
+                    <h6>Lesson</h6>
+                    <p>48</p>
+                  </div>
+                  </div>
+                  <div className="timeStudyItem">
+                  <SwitcherOutlined />
+                  <div>
+                    <h6>Learning ability</h6>
+                    <p>Good</p>
+                  </div>
+                  </div>
+                  <div className="timeStudyItem">
+                  <LayoutOutlined />
+                  <div>
+                    <h6>Total score</h6>
+                    <p>80</p>
+                  </div>
+                  </div>
+                  <div className="timeStudyItem">
+                  <BarChartOutlined />
+                  <div>
+                    <h6>Study of hours</h6>
+                    <p>80</p>
+                  </div>
+                  </div>
+                  <div className="timeStudyItem">
+                  <FileProtectOutlined />
+                  <div>
+                    <h6>Exercise</h6>
+                    <p>80</p>
+                  </div>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
