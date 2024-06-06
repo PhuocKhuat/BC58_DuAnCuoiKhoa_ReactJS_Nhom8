782001b131a6e6d387691776bcff2ce24f9f39a5
Phuoc <khuathoduyphuoc2001@gmail.com>
2024-06-06 10:49:47 +0700

    Optimize infoUser attribute in PersonalInfo component

--- src/Pages/PersonalInfo/PersonalInfo.js
+++ src/Pages/PersonalInfo/PersonalInfo.js
@@ -22,7 +22,7 @@ export default function PersonalInfo() {
   }, []);
   // const informationUser = useMemo(() => {
   //   return infoUser;
-  // }, []);
+  // }, [infoUser]);
 
   return (
     <div className="text-gray-600 body-font personalInfo">
